27ebc18503e45abd311e24017b68d1eee946c6d3
reskyvogue <reskybagja@gmail.com>
2017-08-24 17:23:01 +0700

    done

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link href="style.css" media="all" rel="Stylesheet" type="text/css" /> 
+</head>
+<body>
+<h1 class='title'>Happy birthday Ega Ningsih..</h1>
+<div class='cake'>
+  <div class='candles'></div>
+</div>
+<p>
+  Semoga panjang umur, sehat selalu, dan dalam lindungan Allah SWT. aamiin
+  <a href='https://github.com/jaicab' target='_blank'></a>
+</p>
+<center>
+We Love You..</br>
+Resky Bagja S. & dede utun
+</center>
+</body>
+</html>
+
+
